05dcb9857d05612d8afbfec694af0735adba1ab2
fcardenas07 <f.cardenasreyes.94@gmail.com>
2023-04-12 09:22:08 -0400

    Se implementa primer punto operaciones básicas, mayor, menor, potencia

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -2,4 +2,37 @@ public class Calculadora {
     public static void main(String[] args) {
 
     }
+
+    public static int sumar(int numero1, int numero2) {
+        return numero1 + numero2;
+    }
+
+    public static int restar(int numero1, int numero2) {
+        return numero1 - numero2;
+    }
+
+    public static int multiplicar(int numero1, int numero2) {
+        return numero1 * numero2;
+    }
+
+    public static int dividir(int numero1, int numero2) {
+        return numero1 - numero2;
+    }
+
+    public static int mayor(int numero1, int numero2) {
+        return Math.max(numero1, numero2);
+    }
+
+    public static int menor(int numero1, int numero2) {
+        return Math.min(numero1, numero2);
+    }
+
+    public static double elevarNumero(double numero1, double numero2) {
+        return Math.pow(numero1, numero2);
+    }
+
+    public static double porcentaje(double numero, double porcentaje) {
+        return (porcentaje * numero) / 100;
+    }
+
 }
