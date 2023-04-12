f8ab84384909e19dcbc04e5698434831e84a5a90
thomas <t.kinderman01@ufromail.cl>
2023-04-12 10:12:10 -0400

    implementación item 5

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -1,5 +1,15 @@
 public class Calculadora {
     public static void main(String[] args) {
-
     }
+
+    public static String calcularEcuacionRecta(double[] x1, double[] x2) {
+        double m = (x1[1] - x2[1]) / (x1[0] - x2[0]);
+        if(m!=0) {
+            return "y = " + m + "x + " + (x1[1] - m * x1[0]);
+        }else{
+            return "y = "+x1[1];
+        }
+    }
+
+
 }
--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -1,5 +1,22 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculadoraTest {
+
+    @Test
+    void calcularEcuacionRectaTest() {
+        double[] x1 = {3, 2};
+        double[] x2 = {2, 1};
+        assertEquals("y = 1.0x + -1.0", Calculadora.calcularEcuacionRecta(x1, x2));
+    }
+
+    @Test
+    void calcularEcuacionRectaPendienteCero() {
+        double[] x1 = {2, 5};
+        double[] x2 = {5, 5};
+        assertEquals("y = 5.0", Calculadora.calcularEcuacionRecta(x1,x2));
+    }
 
 }
